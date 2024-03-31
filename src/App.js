c0f3fa235ceef05fa87b32a3726c176fb563ea44
Shekhar <shekharsuman689@gmail.com>
2024-03-31 12:35:27 +0530

    React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="logo-box">
+      <img src="./logo.png" alt="apple" className="logo" />
+      <h2>Sign In to Twitter</h2>
+      <button>
+        <img src="./google.png" alt="apple" />
+        Sign in with Google
+      </button>
+      <button>
+        <img src="./apple.png" alt="apple" />
+        Sign in with Apple
+      </button>
+      <hr></hr>
+      <span>Or</span>
+      <form>
+        <input type="text" placeholder="Phone email or username" />
+        <button>Next</button>
+      </form>
+      <button>Forget Password</button>
+      <p>
+        Don't Have an account <a href="/signup">Sign up</a>
+      </p>
     </div>
   );
 }
-
+ 
 export default App;
